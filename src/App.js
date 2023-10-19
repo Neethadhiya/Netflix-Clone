a7917f74acd1eedf9dfab63919f080ff078919a1
unknown <neethadhiya@gmail.com>
2023-10-19 18:11:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import axios from 'axios'
+import {action,originals} from './urls'
+import Navbar from './Components/Navbar/Navbar'
+import './App.css'
+import Banner from './Components/Banner/Banner'
+import RawPost from './Components/RawPost/RawPost'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      <Banner/>
+      <RawPost url={originals} title='Netflix Originals'/>
+      <RawPost url={action} title='Action' isSmall/>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
